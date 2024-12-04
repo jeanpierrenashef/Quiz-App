df13c80689a18b33ad5ca13c5c0c3987df06d71d
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-04 22:23:26 +0300

    Displayed quizes page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import "../styles/Quizes.css";
 
 
 const Home = () => {
-
+    const navigate = useNavigate()
     const quizes = useSelector((global)=>global.quizes);
-    console.log(quizes);
-
-  return (
-    <div>
-      
+    
+return (
+  <div className="quizes">
+    <h1>Quizzes</h1>
+    <div className="quiz-card">
+      {quizes.list.map((q) => (
+        <div className="quiz-item">
+          <h3>{q.title}</h3>
+          <button
+            onClick={() => {
+              navigate(`/quize/${q.id}`);
+            }}
+          >
+            Start
+          </button>
+        </div>
+      ))}
     </div>
-  )
+  </div>
+);
+    
+    
 }
 
 export default Home
