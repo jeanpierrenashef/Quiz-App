9b1d4ac77d431e734c7fc4961c6e25be84e765aa
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-04 23:52:20 +0300

    Handling the answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,33 +5,40 @@ import { useSelector } from "react-redux";
 const Quiz = () => {
     const {id} = useParams();
     const quizzes = useSelector((global) => global.quizes);
+    const [userAnswers, setUserAnswers] = useState({});
 
     const quiz = quizzes.list.find((q) => q.id === Number(id));
 
 if (!quiz) {
     return <h2>Quiz not found</h2>; 
 }
-
+    const handleAnswers = (questionId, value) => {
+        setUserAnswers((prev)=>({
+            ...prev,
+            [questionId]:value,
+        }));
+    }
 return (
     <div className="quiz">
-    <h1>{quiz.title}</h1>
-    <div className="questions">
-        {quiz.questions.map((question) => (
-        <div className="question">
-            <h3>{question.text}</h3>
-            {question.type === "multiple-choice" ? (
-            <ul>
-                {question.options.map((option, i) => (
-                <li key={i}>{option}</li>
+        <h1>{quiz.title}</h1>
+        <div className="questions">
+        {quiz.questions.map((q) => ( 
+            <div key={q.id} className="question">
+            <h3>{q.text}</h3>
+            {q.type === "multiple-choice" ? (
+                <ul>
+                {q.options.map((option, i) => ( 
+                    <li key={i}>{option}</li>
                 ))}
-            </ul>
+                </ul>
             ) : (
-            <input type="text" placeholder="Your answer" />
+                <input type="text" placeholder="Your answer" />
             )}
-        </div>
+            </div>
         ))}
-    </div>
+        </div>
     </div>
 );
+
 }
 export default Quiz;
